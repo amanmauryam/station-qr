136054a1de62a88d3b293bacc769098005fca89e
amanmauryam <157608609+amanmauryam@users.noreply.github.com>
2026-05-18 11:01:59 +0530

    Fix Prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,13 @@
-import "dotenv/config";
-
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL!;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaPg({
-  connectionString,
-});
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
 
-export const prisma = new PrismaClient({
-  adapter,
-});
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
